168687be5163ca895e2602a6704ab4699f71b3a2
raghu <raghumina11@gmail.com>
2020-07-16 19:37:06 +0530

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,6 +1,7 @@
 # PROBLEMS FROM HACKEREARTH
 # PROBLEMS FOR PRACTICE
 
+# PROBLEM 1 JADOO AND KOBA
 
 # Jadoo, the space alien has challenged Koba to a friendly duel.
 # He asks Koba to write a program to print out all numbers from 70 to 80.
@@ -8,9 +9,9 @@
 # He asks Koba to write the program without using a single number in his program
 # and also restricts the character limit to 100.
 
+# Solution
 
-number = 70
-for number in range(70,80):
+for number in range(69,80):
     number = number + 1
     print(number )
 
